83176fee16e4efd40f6aa39d57d720c2a0e2861c
Utopia <53878781+RomanGoddess@users.noreply.github.com>
2020-09-14 05:58:55 +0100

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
 START_MSG = """
-**Hi {fullname},**
+**Hi {},**
 
 **I'm a simple inline app searching bot which helps you to search and share applications.\nHit the go inline button or call me from any chat just by typing my username in the text field.\nNB: Make sure the name of the app you are looking for is correct else bot cannot index it while searching**.
 """
