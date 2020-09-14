5c21be1af7e4e2722798c2acabdb90354970d2ba
Utopia <53878781+RomanGoddess@users.noreply.github.com>
2020-09-14 05:47:37 +0100

    Update info.py

--- info.py
+++ info.py
@@ -22,9 +22,9 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
 START_MSG = """
-**Hi {}, I'm Modzilla Apk Bot**
+**Hi {username},**
 
-I'm a simple inline app searching bot which helps you to search and share applications.\nHit the go inline button or call me from any chat just by typing my username in the text field.\nNB: Make sure the name of the app you are looking for is correct else bot cannot index it while searching.
+**I'm a simple inline app searching bot which helps you to search and share applications.\nHit the go inline button or call me from any chat just by typing my username in the text field.\nNB: Make sure the name of the app you are looking for is correct else bot cannot index it while searching**.
 """
 
 SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
