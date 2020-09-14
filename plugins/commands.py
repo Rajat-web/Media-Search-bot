8286001c74947228ddda36bbeaecb63f96d6f140
Utopia <53878781+RomanGoddess@users.noreply.github.com>
2020-09-14 05:51:59 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -13,7 +13,7 @@ async def start(bot, message):
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await message.reply(
-        text=START_MSG.format(username=bot.username),
+        text=START_MSG.format(username=user.fullname),
         reply_markup=reply_markup)
 
 
