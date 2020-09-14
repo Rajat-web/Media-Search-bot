4059c4f015801239aad2c60b90383d6739c5b909
Utopia <53878781+RomanGoddess@users.noreply.github.com>
2020-09-14 06:02:26 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,6 +12,7 @@ async def start(bot, message):
         InlineKeyboardButton('Go Inline', switch_inline_query=''),
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
+    chat = update.message.chat
     fullname = f"{chat.first_name} {chat.last_name}" if chat.last_name else chat.first_name
     await message.reply(
         text=START_MSG.format(fullname),
