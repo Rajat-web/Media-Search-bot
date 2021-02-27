9fbecce4905e68e99d82a0eec47b24ff9180c33a
女神 <53878781+RomanGoddess@users.noreply.github.com>
2021-02-27 08:29:32 +0100

    Update info.py

--- info.py
+++ info.py
@@ -26,9 +26,9 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
 START_MSG = """
-**Hi {username}, Welcome To Modzilla Apk Bot**
+**Hi {username}, Welcome To Barn Series Bot**
 
-**I'm a simple inline app searching bot which helps you to search and share android Premium/Modded Applications from all across Telegram.\nHit the go inline button or call me from any chat just by typing my username in the text field.\nNB: Make sure the name of the application you are looking for is correct else bot cannot index it while searching.**
+**I'm a simple inline movie searching bot which helps you to search and share series from all across Telegram.\nHit the go inline button or call me from any chat just by typing my username in the text field.\nNB: Make sure the name of the series you are looking for is correct else bot cannot index it while searching.**
 """
 
-SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
+SHARE_BUTTON_TEXT = 'Checkout {username} for searching series'
