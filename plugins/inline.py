7f84e81b7d7fde19e3efdfa37261a93ceba3310d
KrabbiePattie <70745660+KrabbiePattie@users.noreply.github.com>
2022-02-19 17:43:22 +0100

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -77,7 +77,7 @@ async def answer(bot, query):
 
 def get_reply_markup(username, query):
     url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
-    buttons = [[InlineKeyboardButton('Search Here', switch_inline_query_current_chat='')], [InlineKeyboardButton('Go Inline', switch_inline_query='')]]
+    buttons = [[InlineKeyboardButton('Search Again', switch_inline_query_current_chat=query)], [InlineKeyboardButton('Share Bot', url=url)]]
     return InlineKeyboardMarkup(buttons)
 
 
